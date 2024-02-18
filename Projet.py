d035d822b46b5eb5416e9dce46dc71f72eccf307
InsafNa <insafloussaief773@gmail.com>
2024-02-18 15:24:32 +0100

    4 commit

--- Projet.py
+++ Projet.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-from PIL import ImageTk, Image
 import paho.mqtt.client as mqtt
 
 class MQTTTestApp:
@@ -20,27 +19,41 @@ class MQTTTestApp:
         self.connect_to_mqtt()
 
     def create_gui(self):
-        # Charger l'image pour l'arrière-plan
-        self.bg_image = Image.open("bg1.jpg")
-        self.bg_photo = ImageTk.PhotoImage(self.bg_image)
+          # Configuration de la couleur d'arrière-plan
+          
+        bg_color = "#98bad5"
+        self.root.configure(bg=bg_color)
+        
+        padx_value = 20
+        pady_value = 10
         
         # Label pour afficher les données
-        self.motion_label = ttk.Label(self.root, text="Mouvement: ")
-        self.motion_label.pack(pady=10)
+        label_style1 = {'font': ('Arial', 18), 'foreground': '#3350FF', 'background': bg_color, 'anchor': 'w'}
 
-        self.light_label = ttk.Label(self.root, text="Luminosité: ")
-        self.light_label.pack(pady=10)
+        label_style = {'font': ('Arial', 12), 'foreground': 'black', 'background': bg_color, 'anchor': 'w'}
+        
+         # Titre des données captées par les capteurs
+        self.title_label = ttk.Label(self.root, text=" Données captées ", **label_style1)
+        self.title_label.pack(pady=pady_value, padx=padx_value)
+        
+        self.motion_label = ttk.Label(self.root, text="Mouvement: ", **label_style)
+        self.motion_label.pack(pady=(pady_value, 0), padx=padx_value, anchor="w")
 
-        self.temperature_label = ttk.Label(self.root, text="Température: ")
-        self.temperature_label.pack(pady=10)
+        self.light_label = ttk.Label(self.root, text="Luminosité: ", **label_style)
+        self.light_label.pack(pady=pady_value, padx=padx_value, anchor="w")
 
+        self.temperature_label = ttk.Label(self.root, text="Température: ", **label_style)
+        self.temperature_label.pack(pady=pady_value, padx=padx_value, anchor="w")
+
+         # Conteneur de cadre pour les boutons
+        button_frame = ttk.Frame(self.root, padding=(0, pady_value))
+        button_frame.pack()
         # Boutons pour activer et désactiver le relais
         self.activate_button = ttk.Button(self.root, text="Activer Relais", command=self.activate_relay)
-        self.activate_button.pack(pady=10)
+        self.activate_button.pack(side="left", padx=(100, padx_value), pady=pady_value)
 
         self.deactivate_button = ttk.Button(self.root, text="Désactiver Relais", command=self.deactivate_relay)
-        self.deactivate_button.pack(pady=10)
-
+        self.deactivate_button.pack(side="left", padx=(0, padx_value), pady=pady_value)
     def connect_to_mqtt(self):
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
